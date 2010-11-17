95d04ac42468fbaca36199828a89bb8c20dfa073
Katrina Payne <FullMetalHarlot@NIMHLabs.Com>
2010-11-16 19:02:28 -0700

    constants file still sucks... big time

--- Includes/ClassFile/Constants.h
+++ Includes/ClassFile/Constants.h
@@ -1,35 +1,44 @@
 /*nimh-doc
  *= Includes/ClassFile/Constants.h
  * Author: %(KatrinaTheLamia)user
- * Project: %(VirtuaGirl)project %(Jerl6)project %(Jython3)project %(HTCemu)project
+ * Project: %(JavaKitty)project %(Jerl6)project %(Jython3)project %(HTCemu)project
  * Groups: %(NIMHLabs) %(Perl6) %(RaddTeam) %(TeamTemporal)
  * Creation: 3176-2-62
- * Revisions:
- * + 3176-2-62 Initial Creation
- * Purpose:
+ *
  * Define the in memory information in the Constants section of the class file
+ *
+ * == Revisions:
+ * + 3176-2-62 Initial Creation
+ * ~ 3176-5-28 Update the comments to fit the proper name
+ * ! 3176-5-28 File still dangerously stupid
+ *
+ * == TODO
+ * ! Make file suck a lot less
+ *
  */
 
-enum VG_Constant_Type {
-	vg_t_utf8 = 1,
-	vg_t_interger = 3,
-	vg_t_float = 4,
-	vg_t_long = 5,
-	vg_t_double = 6,
-	vg_t_class = 7,
-	vg_t_string = 8,
-	vg_t_field_reference = 9,
-	vg_t_method_reference = 10,
-	vg_t_interface_method_reference = 11,
-	vg_t_name_and_type = 12,
+enum JK_Constant_Type {
+	jk_t_utf8 = 1,
+	jk_t_interger = 3,
+	jk_t_float = 4,
+	jk_t_long = 5,
+	jk_t_double = 6,
+	jk_t_class = 7,
+	jk_t_string = 8,
+	jk_t_field_reference = 9,
+	jk_t_method_reference = 10,
+	jk_t_interface_method_reference = 11,
+	jk_t_name_and_type = 12,
 };
 
 /*
  * Quick notes before passing out:
  * Since this is meant for internal memory purposes, the internals to the 
  * constant will be elaborated elsewhere
+ *
+ *
  */
 typedef struct {
-	VG_Constant_Type tag;
+	JK_Constant_Type tag;
 	void *constant_internals;
-} VG_Constant_dat VG_Constant;
+} JK_Constant_dat JK_Constant;
