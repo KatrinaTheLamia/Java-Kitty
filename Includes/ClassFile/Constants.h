fcb72e024116a2f16bcfaed58e550c5941321bae
Katrina Payne <FullMetalHarlot@NIMHLabs.Com>
2010-11-18 17:17:27 -0700

    minor changes. Something shiny kept appearing during this update

--- Includes/ClassFile/Constants.h
+++ Includes/ClassFile/Constants.h
@@ -11,13 +11,37 @@
  * + 3176-2-62 Initial Creation
  * ~ 3176-5-28 Update the comments to fit the proper name
  * ! 3176-5-28 File still dangerously stupid
+ * + 3176-5-30 Access constants added
+ * + 3176-5-30 Fixed header file issues
+ * + 3176-5-30 Add guards for C++
+ * ~ 3176-5-30 Made file less retarded
  *
  * == TODO
  * ! Make file suck a lot less
  *
  */
 
-enum JK_Constant_Type {
+
+/*
+ * Start header
+ */
+#ifndef __JK_ClassFile_Constants_H__
+#define __JK_ClassFile_Constants_H__
+
+/*
+ * C++ Guards, naturally.
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ *
+ * An enum for numerical type constants.
+ *
+ */
+enum JK_Constant_Type 
+{
 	jk_t_utf8 = 1,
 	jk_t_interger = 3,
 	jk_t_float = 4,
@@ -32,13 +56,32 @@ enum JK_Constant_Type {
 };
 
 /*
- * Quick notes before passing out:
- * Since this is meant for internal memory purposes, the internals to the 
- * constant will be elaborated elsewhere
- *
+ * Access constants
+ * Flags for Class Files to dictate access.
+ */
+
+enum JK_Constant_Type 
+{
+	jk_access_is_public = 0x0001,
+	jk_access_is_finale = 0x0010,
+	jk_access_is_super = 0x0020,
+	jk_access_is_interface = 0x0100,
+	jk_access_is_abstract = 0x0200,
+};
+
+
+/*
+ * The actual varients of Constants stilll need to be defined
  *
  */
-typedef struct {
+typedef struct 
+{
 	JK_Constant_Type tag;
 	void *constant_internals;
 } JK_Constant_dat JK_Constant;
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* End the header */
